86e9accdf703e586c2d2d5fa3fad5d9dba5b2cdc
shagun9999 <110155117+shagun9999@users.noreply.github.com>
2022-08-02 14:21:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PIL import Image
 import pickle
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding', False)
 # Load the pickled model
